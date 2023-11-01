4fbe19e308747df19a1322cdc5a03660f91881b2
Juan.M <125684877+JuanMM0@users.noreply.github.com>
2023-11-01 14:15:11 -0300

    Add files via upload

--- TestaVeiculo.java
+++ TestaVeiculo.java
@@ -1,13 +1,209 @@
+import java.util.Scanner;
+
 public class TestaVeiculo {
     public static void main(String[] args) {
-        Veiculo carro = new Carro("Chevrolett", 250.0);
-        Veiculo bicicleta = new Bicicleta("Monark", 60);
+        Locadora locadora = new Locadora();
+        Scanner scanner = new Scanner(System.in);
 
-        Veiculo[] veiculos = {carro, bicicleta};
-        for (Veiculo veiculo : veiculos) {
-            veiculo.imprimirDescricao();
-            System.out.println("Velocidade máxima: " + veiculo.calVLMax() + " km/h");
-            System.out.println();
+        while (true) {
+            System.out.println("\nSistema de Gerenciamento de Veículos");
+            System.out.println("1. Cadastrar veículo");
+            System.out.println("2. Cadastrar cliente");
+            System.out.println("3. Alugar veículo");
+            System.out.println("4. Devolver veículo");
+            System.out.println("5. Abastecer veículo");
+            System.out.println("6. Consultar informações");
+            System.out.println("7. Listar clientes");
+            System.out.println("8. Calcular valor do aluguel");
+            System.out.println("9. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    // Cadastrar veículo
+                    System.out.print("Marca do veículo: ");
+                    String marcaVeiculo = scanner.nextLine();
+                    System.out.print("Modelo do veículo: ");
+                    String modeloVeiculo = scanner.nextLine();
+                    System.out.print("Ano do veículo: ");
+                    int anoVeiculo = scanner.nextInt();
+                    System.out.print("Quantidade de combustível: ");
+                    double quantidadeCombustivel = scanner.nextDouble();
+                    System.out.print("Preço da diária: ");
+                    double precoDiaria = scanner.nextDouble();
+
+                    System.out.println("Tipo de veículo (1 - Carro, 2 - Moto): ");
+                    int tipoVeiculo = scanner.nextInt();
+                    scanner.nextLine();
+
+                    Veiculo novoVeiculo;
+
+                    if (tipoVeiculo == 1) {
+                        System.out.print("Número de portas: ");
+                        int numeroPortas = scanner.nextInt();
+                        novoVeiculo = new Carro(marcaVeiculo, modeloVeiculo, anoVeiculo, quantidadeCombustivel, numeroPortas, precoDiaria);
+                    } else if (tipoVeiculo == 2) {
+                        System.out.print("Cilindrada: ");
+                        int cilindrada = scanner.nextInt();
+                        novoVeiculo = new Moto(marcaVeiculo, modeloVeiculo, anoVeiculo, quantidadeCombustivel, cilindrada, precoDiaria);
+                    } else {
+                        System.out.println("Opção inválida. Veículo não cadastrado.");
+                        continue;
+                    }
+
+                    locadora.cadastrarVeiculo(novoVeiculo);
+                    break;
+
+                case 2:
+                    // Cadastrar cliente
+                    System.out.print("Nome do cliente: ");
+                    String nomeCliente = scanner.nextLine();
+                    System.out.print("Idade do cliente: ");
+                    int idadeCliente = scanner.nextInt();
+                    scanner.nextLine();
+                    Cliente novoCliente = new Cliente(nomeCliente, idadeCliente);
+                    locadora.cadastrarCliente(novoCliente);
+                    break;
+
+                case 3:
+                    // Alugar veículo
+                    System.out.print("Nome do cliente: ");
+                    String nomeAlugar = scanner.nextLine();
+                    System.out.print("Marca do veículo: ");
+                    String marcaAlugar = scanner.nextLine();
+                    System.out.print("Modelo do veículo: ");
+                    String modeloAlugar = scanner.nextLine();
+
+                    Cliente clienteAlugar = null;
+
+                    for (Cliente cliente : locadora.clientes) {
+                        if (cliente.getNome().equalsIgnoreCase(nomeAlugar)) {
+                            clienteAlugar = cliente;
+                            break;
+                        }
+                    }
+
+                    if (clienteAlugar != null) {
+                        Veiculo veiculoAlugar = null;
+                        for (Veiculo veiculo : locadora.veiculos) {
+                            if (veiculo.getMarca().equalsIgnoreCase(marcaAlugar) && veiculo.getModelo().equalsIgnoreCase(modeloAlugar)) {
+                                veiculoAlugar = veiculo;
+                                break;
+                            }
+                        }
+
+                        if (veiculoAlugar != null) {
+                            locadora.alugarVeiculo(clienteAlugar, veiculoAlugar);
+                        } else {
+                            System.out.println("Veículo não encontrado.");
+                        }
+                    } else {
+                        System.out.println("Cliente não encontrado.");
+                    }
+                    break;
+
+                case 4:
+                    // Devolver veículo
+                    System.out.print("Nome do cliente: ");
+                    String nomeDevolver = scanner.nextLine();
+                    Cliente clienteDevolver = null;
+
+                    for (Cliente cliente : locadora.clientes) {
+                        if (cliente.getNome().equalsIgnoreCase(nomeDevolver)) {
+                            clienteDevolver = cliente;
+                            break;
+                        }
+                    }
+
+                    if (clienteDevolver != null) {
+                        locadora.devolverVeiculo(clienteDevolver);
+                    } else {
+                        System.out.println("Cliente não encontrado.");
+                    }
+                    break;
+
+                case 5:
+                    // Abastecer veículo
+                    System.out.print("Nome do cliente: ");
+                    String nomeAbastecer = scanner.nextLine();
+                    System.out.print("Quantidade de combustível: ");
+                    double quantidadeAbastecer = scanner.nextDouble();
+                    Cliente clienteAbastecer = null;
+
+                    for (Cliente cliente : locadora.clientes) {
+                        if (cliente.getNome().equalsIgnoreCase(nomeAbastecer)) {
+                            clienteAbastecer = cliente;
+                            break;
+                        }
+                    }
+
+                    if (clienteAbastecer != null) {
+                        locadora.abastecerVeiculo(clienteAbastecer, quantidadeAbastecer);
+                    } else {
+                        System.out.println("Cliente não encontrado.");
+                    }
+                    break;
+
+                case 6:
+                    // Consultar informações
+                    System.out.print("Nome do cliente: ");
+                    String nomeConsultar = scanner.nextLine();
+                    Cliente clienteConsultar = null;
+
+                    for (Cliente cliente : locadora.clientes) {
+                        if (cliente.getNome().equalsIgnoreCase(nomeConsultar)) {
+                            clienteConsultar = cliente;
+                            break;
+                        }
+                    }
+
+                    if (clienteConsultar != null) {
+                        locadora.consultarInformacoes(clienteConsultar);
+                    } else {
+                        System.out.println("Cliente não encontrado.");
+                    }
+                    break;
+
+                case 7:
+                    // Listar clientes
+                    locadora.listarClientes();
+                    break;
+
+                case 8:
+                    // Calcular valor do aluguel
+                    System.out.print("Nome do cliente: ");
+                    String nomeCalcularAluguel = scanner.nextLine();
+                    System.out.print("Número de dias de aluguel: ");
+                    int diasAluguel = scanner.nextInt();
+                    Cliente clienteCalcularAluguel = null;
+
+                    for (Cliente cliente : locadora.clientes) {
+                        if (cliente.getNome().equalsIgnoreCase(nomeCalcularAluguel)) {
+                            clienteCalcularAluguel = cliente;
+                            break;
+                        }
+                    }
+
+                    if (clienteCalcularAluguel != null) {
+                        clienteCalcularAluguel.calcularAluguel(diasAluguel);
+                    } else {
+                        System.out.println("Cliente não encontrado.");
+                    }
+                    break;
+
+                case 9:
+                    // Sair do programa
+                    System.out.println("O programa foi encerrado.");
+                    scanner.close();
+                    System.exit(0);
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
         }
     }
 }
--- Veiculo.java
+++ Veiculo.java
@@ -1,43 +1,212 @@
+import java.util.ArrayList;
+import java.util.List;
+
 abstract class Veiculo {
-    protected String marca;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private double quantidadeCombustivel;
+    private double precoDiaria;
+    
+    
 
-    public Veiculo(String marca) {
+    public Veiculo(String marca, String modelo, int ano, double quantidadeCombustivel, double precoDiaria) {
         this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.quantidadeCombustivel = quantidadeCombustivel;
+        this.precoDiaria = precoDiaria;
     }
-    public abstract double calVLMax();
-    public abstract void imprimirDescricao();
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public double getQuantidadeCombustivel() {
+        return quantidadeCombustivel;
+    }
+
+    public double getPrecoDiaria() {
+        return precoDiaria;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Marca: " + marca + ", Modelo: " + modelo + ", Ano: " + ano + ", Combustível: " + quantidadeCombustivel;
+    }
+
+    public void setQuantidadeCombustivel(double litros) {
+    }
 }
+
 class Carro extends Veiculo {
-    private double potencia;
+    private int numeroPortas;
 
-    public Carro(String marca, double potencia) {
-        super(marca);
-        this.potencia = potencia;
+    public Carro(String marca, String modelo, int ano, double quantidadeCombustivel, int numeroPortas, double precoDiaria) {
+        super(marca, modelo, ano, quantidadeCombustivel, precoDiaria);
+        this.numeroPortas = numeroPortas;
     }
-    @Override
-    public double calVLMax() {
-        // Exemplo de cálculo de velocidade máxima para um carro
-        return potencia * 2.0;
+
+    public int getNumeroPortas() {
+        return numeroPortas;
     }
+
+   
+
     @Override
-    public void imprimirDescricao() {
-        System.out.println("Carro da marca " + marca);
+    public String toString() {
+        return super.toString() + ", Número de Portas: " + numeroPortas;
     }
 }
-class Bicicleta extends Veiculo {
-    private int forcaCiclista;
 
-    public Bicicleta(String marca, int forcaCiclista) {
-        super(marca);
-        this.forcaCiclista = forcaCiclista;
+class Moto extends Veiculo {
+    private int cilindrada;
+
+    public Moto(String marca, String modelo, int ano, double quantidadeCombustivel, int cilindrada, double precoDiaria) {
+        super(marca, modelo, ano, quantidadeCombustivel, precoDiaria);
+        this.cilindrada = cilindrada;
     }
-    @Override
-    public double calVLMax() {
-        // Exemplo de cálculo de velocidade máxima para uma bicicleta
-        return forcaCiclista * 0.2;
+
+    public int getCilindrada() {
+        return cilindrada;
     }
+
     @Override
-    public void imprimirDescricao() {
-        System.out.println("Bicicleta da marca " + marca);
+    public String toString() {
+        return super.toString() + ", Cilindrada: " + cilindrada;
     }
 }
+
+interface OperacoesVeiculo {
+    void alugarVeiculo(Veiculo veiculo);
+
+    void devolverVeiculo();
+
+    void abastecerVeiculo(double quantidade);
+
+    void consultarInformacoes();
+
+    double calcularAluguel(int dias);
+}
+
+class Cliente implements OperacoesVeiculo {
+    private String nome;
+    private int idade;
+    private Veiculo veiculoAlugado;
+    private double valorTotalAluguel;
+
+    public Cliente(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public void alugarVeiculo(Veiculo veiculo) {
+        if (veiculoAlugado == null) {
+            veiculoAlugado = veiculo;
+            System.out.println("Veículo alugado: " + veiculo);
+        } else {
+            System.out.println("Você já possui um veículo alugado.");
+        }
+    }
+
+    @Override
+    public void devolverVeiculo() {
+        if (veiculoAlugado != null) {
+            System.out.println("Veículo devolvido: " + veiculoAlugado);
+            veiculoAlugado = null;
+        } else {
+            System.out.println("Você não possui um veículo alugado para devolver.");
+        }
+    }
+  @Override
+    public void abastecerVeiculo(double quantidade) {
+    if (veiculoAlugado != null) {
+        double combustivelAtual = veiculoAlugado.getQuantidadeCombustivel();
+        veiculoAlugado.setQuantidadeCombustivel(combustivelAtual + quantidade);
+        System.out.println("Veículo abastecido. Novo nível de combustível: " + veiculoAlugado.getQuantidadeCombustivel());
+    } else {
+        System.out.println("Você não possui um veículo alugado para abastecer.");
+    }
+}
+
+
+    @Override
+    public void consultarInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        if (veiculoAlugado != null) {
+            System.out.println("Veículo alugado: " + veiculoAlugado);
+        } else {
+            System.out.println("Não há veículo alugado no momento.");
+        }
+    }
+
+    @Override
+    public double calcularAluguel(int dias) {
+        if (veiculoAlugado != null) {
+            valorTotalAluguel = veiculoAlugado.getPrecoDiaria() * dias;
+            System.out.println("Valor total do aluguel: R$" + valorTotalAluguel);
+            return valorTotalAluguel;
+        } else {
+            System.out.println("Você não possui um veículo alugado para calcular o valor do aluguel.");
+            return 0.0;
+        }
+    }
+}
+
+class Locadora {
+    List<Veiculo> veiculos;
+    List<Cliente> clientes;
+
+    public Locadora() {
+        veiculos = new ArrayList<>();
+        clientes = new ArrayList<>();
+    }
+
+    public void cadastrarVeiculo(Veiculo veiculo) {
+        veiculos.add(veiculo);
+        System.out.println("Veículo cadastrado: " + veiculo);
+    }
+
+    public void cadastrarCliente(Cliente cliente) {
+        clientes.add(cliente);
+        System.out.println("Cliente cadastrado: " + cliente.getNome());
+    }
+
+    public void alugarVeiculo(Cliente cliente, Veiculo veiculo) {
+        cliente.alugarVeiculo(veiculo);
+    }
+
+    public void devolverVeiculo(Cliente cliente) {
+        cliente.devolverVeiculo();
+    }
+
+    public void abastecerVeiculo(Cliente cliente, double quantidade) {
+        cliente.abastecerVeiculo(quantidade);
+    }
+
+    public void consultarInformacoes(Cliente cliente) {
+        cliente.consultarInformacoes();
+    }
+
+    public void listarClientes() {System.out.println("Clientes cadastrados:");
+        for (Cliente cliente : clientes) {
+            System.out.println(cliente.getNome());
+        }
+    }
+}
+
